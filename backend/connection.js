fa900e452c48ea90b77a5ef0b27ccb18a6579d5e
kennedy muthuri <muthurikennedy082@gmail.com>
2022-10-03 21:31:32 +0300

    editing js

--- backend/connection.js
+++ backend/connection.js
@@ -3,4 +3,33 @@ const cors=require('cors');
 const mongoose=require('mongoose');
 const multer= require('multer');
 const bodyParser= require('body-parser');
+const { application } = require('express');
+const PORT=8000;
+
+//initializing express
+const app= express();
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+//mongodb url
+const mogoURL='mongodb://0.0.0.0/todo';
+
+//connectiong to the server and database
+mongoose.connect(mogoURL)
+.then(function(){
+    app.listen(PORT,function(err){
+        if(err){
+            console.log(`Listening error: `,err);
+        }else{
+            console.log(`App connected on port ${PORT}`);
+        }
+    });
+})
+.catch(function(err){
+    console.log('Mongo error: ',err);
+});
+
+//Application Routes
 
---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import{BrowserRouter as Switch, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Routes>
+          <Route/>
+        </Routes>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
